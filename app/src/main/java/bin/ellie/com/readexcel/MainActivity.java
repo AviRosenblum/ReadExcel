4a8aaf25edca049af3a493154097b8c631081b60
Avi Rozenblum <avieli91roz@gmail.com>
2015-08-24 09:35:43 -0400

    Update MainActivity.java

--- app/src/main/java/bin/ellie/com/readexcel/MainActivity.java
+++ app/src/main/java/bin/ellie/com/readexcel/MainActivity.java
@@ -12,6 +12,9 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        // Execute AsynTask
+        new ReadExcel(MainActivity.this).execute("< URL >");
     }
 
     @Override
